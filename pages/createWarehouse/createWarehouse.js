dcca32f4a7051ade084c39c4b259af211589c953
TanYJie <413145873@qq.com>
2019-11-17 16:33:51 +0800

    引入有赞UI并且修改创建仓库的输入框组件

--- pages/createWarehouse/createWarehouse.js
+++ pages/createWarehouse/createWarehouse.js
@@ -5,13 +5,76 @@ Page({
    * 页面的初始数据
    */
   data: {
+    userInfo: {
+      name: '',
+      identity: '',
+      phone: '',
+      email: ''
+    },
+    errorInfo: {
+      nameError: '',
+      identityError: '',
+      phoneError: '',
+      emailError: ''
+    }
+  },
 
+  //处理输入事件
+  inputEventCatcher: function (e) {
+    let {
+      key
+    } = e.target.dataset;
+    let modifyKey = 'userInfo.' + key;
+    this.setData({
+      [modifyKey]: e.detail
+    })
+  },
+
+  checkName: function (name) {
+    let regName = /^[\u4e00-\u9fa5]{2,4}$/;
+    if (!regName.test(name)) {
+      return '姓名格式错误（应为2-4个汉字）';
+    }
+    return '';
+  },
+  checkIdentity: function (identity) {
+    let regIdentity = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;  
+    if (!regIdentity.test(identity)) {
+      return '身份证号格式错误';
+    }
+    return '';
   },
 
   nextStep: function() {
-    wx.navigateTo({
-      url: '../createWarehouse2/createWarehouse2'
+    console.log(this.data.userInfo);
+    let {
+      name,
+      identity,
+      phone,
+      email
+    } = this.data.userInfo;
+
+    // 检测姓名
+    let checkNameResult = this.checkName(name);
+    this.setData({
+      ['errorInfo.nameError']: checkNameResult
     })
+
+    // 检测身份证号
+    let checkIdentityResult = this.checkIdentity(identity);
+    this.setData({
+      ['errorInfo.identityError']: checkIdentityResult
+    })
+
+    // 如果有错，不执行下面的步骤
+    if (checkNameResult.length > 0 || checkIdentityResult.length > 0) {
+      return;
+    }
+
+    // 跳转，这里先注释
+    // wx.navigateTo({
+    //   url: '../createWarehouse2/createWarehouse2'
+    // })
   },
 
   /**
