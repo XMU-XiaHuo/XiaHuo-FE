48aa3c661c6e1eacd0e46882eca21ef3f66d7a14
TanYJie <413145873@qq.com>
2019-11-17 23:10:59 +0800

    创建仓库页面-createWarehouse2输入框组件更改

--- pages/createWarehouse2/createWarehouse2.js
+++ pages/createWarehouse2/createWarehouse2.js
@@ -5,13 +5,55 @@ Page({
    * 页面的初始数据
    */
   data: {
+    warehouseInfo: {
+      name: '',
+      address: '',
+      info: ''
+    },
+    errorInfo: {
+      nameError: '',
+      addressError: '',
+      infoError: ''
+    }
 
   },
 
-  nextStep: function () {
-    wx.navigateTo({
-      url: '../createWarehouse3/createWarehouse3'
+  // 处理输入事件
+  inputEventCatcher: function (e) {
+    let {
+      key
+    } = e.target.dataset;
+    let modifyKey = 'warehouseInfo.' + key;
+    this.setData({
+      [modifyKey]: e.detail
     })
+  },
+
+  checkName: function (name) {
+    let regName = /^[\u4e00-\u9fa5]{2,10}$/;
+    if (!regName.test(name)) {
+      return '仓库名格式错误（应为 2 - 10 个汉字）';
+    }
+    return '';
+  },
+
+  nextStep: function () {
+    let {
+      name,
+      address,
+      info
+    } = this.data.warehouseInfo;
+
+    // 检测仓库名
+    let checkNameResult = this.checkName(name);
+    this.setData({
+      ['errorInfo.nameError']: checkNameResult
+    })
+
+    // 跳转到成功页面
+    // wx.navigateTo({
+    //   url: '../createWarehouse3/createWarehouse3'
+    // })
   },
 
   /**
