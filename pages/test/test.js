9aa8223134e25675022f3c693a712ba31379ed05
TanYJie <413145873@qq.com>
2019-11-26 21:48:55 +0800

    完善封装wxRequest

--- pages/test/test.js
+++ pages/test/test.js
@@ -1,4 +1,8 @@
 // pages/test/test.js
+const {
+  wxRequest
+} = require('../../utils/request.js');
+
 Page({
 
   /**
@@ -11,56 +15,75 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
+    // get 不带参数
+    wxRequest('/get/test', 'GET').then((res) => {
+      console.log(res);
+    });
+    
+    // get 带参数
+    wxRequest('/user', 'GET',{
+      name:'TanYJie'
+    }).then((res) => {
+      console.log(res);
+    });
+
+    // post
+    wxRequest('/post/test', 'POST', {
+      id: 1,
+      test: true
+    }).then((res) => {
+      console.log(res);
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- utils/request.js
+++ utils/request.js
@@ -1,26 +1,30 @@
-const baseURL = 'http://localhost:3000/';
+const baseURL = 'http://localhost:3000';
 
 function wxRequest(
   url = "",
   method = 'GET',
   data = {},
-  header = {},
-  success = (res) => {
-    console.log(res);
-  },
-  fail = (error) => {
-    console.log(error)
-  },
-  complete = () => {},
+  header = {}
 ) {
-  wx.request({
-    url: baseURL + url,
-    data,
-    header: Object.assign({
-      'content-type': 'application/json' // 默认值
-    }, header),
-    success,
-    fail,
-    complete
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: baseURL + url,
+      data,
+      method,
+      header: Object.assign({
+        'content-type': 'application/json' // 默认值
+      }, header),
+      success: (res) => {
+        resolve(res)
+      },
+      fail: (error) => {
+        reject(error)
+      }
+    })
   })
 }
+
+module.exports = {
+  baseURL,
+  wxRequest
+};
